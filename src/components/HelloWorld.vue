7224eeb33c6b17f2048a54997ab40fa21d4ebfde
Mumu99 <xiongpolin@gmail.com>
2020-07-07 17:32:54 +0800

    初始化项目

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <Button type="primary">w</Button>
+    <p class="p2">sss a </p>
+  </div>
+</template>
+
+<script>
+import { Button } from 'vant'
+export default {
+  name: '',
+  components: {
+    // 注册组件
+    Button
+  }
+}
+</script>
+
+<style lang='stylus' rel='stylesheet/stylus' scoped>
+.p2
+  width: 100px
+</style>
